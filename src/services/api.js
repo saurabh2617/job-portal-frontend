20c1e5e9c3c1103706cd27d4126337e5baab322a
saurabh2617 <saurabhingale8170@gmail.com>
2026-03-03 14:33:06 +0530

    Removed .env from tracking

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://job-portal-backend-sa0h.onrender.com/api",
 });
 
 // ✅ Automatically attach JWT token to every request
