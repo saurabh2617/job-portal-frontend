302578de280dc860410824c99cccac81201d01c6
saurabh2617 <saurabhingale8170@gmail.com>
2026-03-03 17:37:40 +0530

    Remove android folder from repository

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.saurabh.jobconnect',
+  appName: 'JobConnect',
+  webDir: 'build'
+};
+
+export default config;
